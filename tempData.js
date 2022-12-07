93f141f40ef74bec6d1651f31ea5f633260eb359
AlonsoAlfa <afaustoalonso@gmail.com>
2022-12-07 11:55:29 -0600

    feat: add new data from de app

---
+++ tempData.js
@@ -0,0 +1,24 @@
+let tempData; export default tempData = [
+    {
+      id: 1,
+      name: 'Plan A Trip',
+      color: '#24A6D9',
+      todos: [
+        {
+          title: 'Book Flight',
+          completed: false
+        },
+        {
+          title: 'Passport Check',
+          completed: false
+        },
+        {
+          title: 'Hotel Room Check',
+          completed: true
+        },
+        {
+          title: 'Pack Luggage',
+          completed: true
+        }
+      ]
+    },
