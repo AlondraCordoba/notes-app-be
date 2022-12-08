840da0e764ac0f6381fab679039ee4bcbd324290
Alumno Administración <aadmon@utags.edu.mx>
2022-12-07 18:00:00 -0600

    Feat: Import code

---
+++ components/TodoModal.js
@@ -0,0 +1,4 @@
+import { Text, View, StyleSheet, SafeAreaView, TouchableOpacity, FlatList, KeyboardAvoidingView, TextInput, Keyboard } from 'react-native'
+import React from 'react';
+import { AntDesign, Ionicons } from '@expo/vector-icons';
+import Colors from '../Colors';
